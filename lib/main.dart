b95f606bd0921a00325ec4f67ec3d3aedc0e1f18
DESKTOP-D7NMFO9\Sandeep <www.sandeepbabu45gmail.com>
2021-04-17 18:42:56 +0530

    Onboarding screen implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_zoom/screen/HomePage.dart';
+import 'package:flutter_zoom/screen/IntroAuthPage.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,6 +9,24 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: NavigationPage(),
+    );
+  }
+}
+
+class NavigationPage extends StatefulWidget {
+  @override
+  _NavigationPageState createState() => _NavigationPageState();
+}
+
+class _NavigationPageState extends State<NavigationPage> {
+  bool isSigned = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: isSigned == false ? IntroAuthPage() : HomePage(),
+    );
   }
 }
---
+++ lib/screen/IntroAuthPage.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_zoom/variable.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+
+class IntroAuthPage extends StatefulWidget {
+  @override
+  _IntroAuthPageState createState() => _IntroAuthPageState();
+}
+
+class _IntroAuthPageState extends State<IntroAuthPage> {
+  @override
+  Widget build(BuildContext context) {
+    return IntroductionScreen(
+      pages: [
+        PageViewModel(
+          title: "Welcome",
+          body: "Welcome to ZoomClone",
+          image: Center(
+            child: Image.asset(
+              'images/welcome.png',
+              height: 175,
+            ),
+          ),
+          decoration: PageDecoration(
+            bodyTextStyle: myStyle(20, Colors.black),
+            titleTextStyle: myStyle(20, Colors.black),
+          ),
+        ),
+        PageViewModel(
+          title: "Join or Start Meeting",
+          body: "Join now",
+          image: Center(
+            child: Image.asset(
+              'images/conference.png',
+              height: 175,
+            ),
+          ),
+          decoration: PageDecoration(
+            bodyTextStyle: myStyle(20, Colors.black),
+            titleTextStyle: myStyle(20, Colors.black),
+          ),
+        ),
+        PageViewModel(
+          title: "Security",
+          body: "Your security is important to us",
+          image: Center(
+            child: Image.asset(
+              'images/secure.jpg',
+              height: 175,
+            ),
+          ),
+          decoration: PageDecoration(
+            bodyTextStyle: myStyle(20, Colors.black),
+            titleTextStyle: myStyle(20, Colors.black),
+          ),
+        ),
+      ],
+      onDone: () {},
+      onSkip: () {},
+      showSkipButton: true,
+      skip: const Icon(
+        Icons.skip_next,
+        size: 45,
+      ),
+      next: const Icon(Icons.arrow_forward_ios),
+      done: Text(
+        'Done',
+        style: myStyle(20, Colors.black),
+      ),
+    );
+  }
+}
